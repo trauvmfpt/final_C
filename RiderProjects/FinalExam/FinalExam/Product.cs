fccaac75044ec3b06f369c4dca2f2e4eadf19607
Tra <trauvmd00621@fpt.edu.vn>
2018-06-26 19:46:17 +0700

    Final exam

---
+++ RiderProjects/FinalExam/FinalExam/Product.cs
@@ -0,0 +1,34 @@
+﻿namespace FinalExam
+{
+    public class Product
+    {
+        private string productId;
+        private string name;
+        private decimal price;
+
+        public Product(string productId, string name, decimal price)
+        {
+            this.productId = productId;
+            this.name = name;
+            this.price = price;
+        }
+
+        public string ProductId
+        {
+            get => productId;
+            set => productId = value;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public decimal Price
+        {
+            get => price;
+            set => price = value;
+        }
+    }
+}
